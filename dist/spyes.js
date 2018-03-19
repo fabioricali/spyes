5b8ff16fe9c39c142efbdf34f78e8eff556abe1e
fabioricali <ricalifabio@gmail.com>
2018-03-19 12:32:35 +0100

    v1.0.1

--- dist/spyes.js
+++ dist/spyes.js
@@ -1,4 +1,4 @@
-// [AIV]  Spyes Build version: 1.0.0  
+// [AIV]  Spyes Build version: 1.0.1  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
